00299e510a1e4ec2728d402286e83d5e939612c0
Pavel Popov <inkvitop@gmail.com>
2021-03-09 15:17:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
+  try {
+    let minNumber = array[0];
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] < minNumber) {
+      minNumber = array[index]
+    }
+  }
+  if (array.length === 0) {
+    return 0
+  }else{
+    return minNumber;
+  }
+  } catch (error) {
+    return 0
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  try {
+    let maxNumber = array[0];
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] > maxNumber) {
+      maxNumber = array[index]
+    }
+  }
+  if (array.length === 0) {
+    return 0
+  }else{
+    return maxNumber;
+  }
+  } catch (error) {
+    return 0
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  try {
+    let summ = 0;
+    let arrayLength = array.length;
+    array.forEach(element => {
+      summ += element;
+    });
+    if (arrayLength === 0) {
+      return 0
+    }else {
+      return summ/arrayLength;
+    }
+  } catch (error) {
+    return 0
+  }
 }
